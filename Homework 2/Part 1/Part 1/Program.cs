94a5af4fe8d892a94f4d22a8dea345c47e56fd71
David S <davidsumanski20@gmail.com>
2022-03-16 13:07:03 +0100

    Homework 2

---
+++ Homework 2/Part 1/Part 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            int sum = 0;
+            Console.WriteLine("Enter 6 elements in array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool element = int.TryParse(Console.ReadLine(), out int num);
+                if (element)
+                {
+                    array[i] = num;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid element!");
+                    i--;
+                }
+            }
+            foreach (int element in array)
+            {
+                if(element % 2 == 0)
+                {
+                    sum += element;
+                }
+            }  
+            Console.WriteLine($"The sum of all even elements in the array is {sum}");
+        }
+    }
+}
---
+++ Homework 2/Part 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsGroupOne = { "Zdravko", "Petko", "Stanko", "Brank", "Trajko"};
+            string[] studentsGroupTwo = { "Zoki", "Mile", "Brus", "Petko", "Vele"};
+            Console.WriteLine("Enter student group (1 or 2)!");
+            bool number = int.TryParse(Console.ReadLine(), out int num);
+            if (number)
+            {
+                if(num == 1)
+                {
+                    Console.WriteLine("Students in group 1:");
+                    foreach(string student in studentsGroupOne)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                if (num == 2)
+                {
+                    Console.WriteLine("Students in group 2:");
+                    foreach (string student in studentsGroupTwo)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                else Console.WriteLine("Invalid group number. There are only 2 groups!");
+            }
+            else Console.WriteLine("Invalid parameter!");
+        }
+    }
+}
